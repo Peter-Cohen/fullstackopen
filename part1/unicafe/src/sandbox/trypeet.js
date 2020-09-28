2c342f187428b7c20c9527138bc16bb11d22680e
Peter-Cohen <peter.cohen.1969@gmail.com>
2020-09-28 13:12:44 +0200

    part1

---
+++ part1/unicafe/src/sandbox/trypeet.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Header = (props) => {
+  return <h1>{props.course}</h1>;
+};
+
+
+const Part = (props) => {
+  return (
+    <p>
+      {' '}
+      {props.part} {props.exercise}
+    </p>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.parts[0].name} exercise={props.parts[0].exercises}
+      />
+      <Part part={props.parts[1].name} exercise={props.parts[1].exercises} />
+      <Part part={props.parts[2].name} exercise={props.parts[2].exercises} />
+    </div>
+  );
+};
+
+
+const Total = (props) => {
+  return (
+    <p>
+      Number of exercises{' '}
+      {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
+    </p>
+  );
+};
+
+
+const App = () => {
+  const course = 'Half Stack application development';
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  )
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
